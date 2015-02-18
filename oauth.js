84ab45143e9213503f3606a136933b0ab07fe064
sgrim3 <susan.grimshaw@students.olin.edu>
2015-02-17 23:02:17 -0500

    corrected oauth link in oath.js

--- oauth.js
+++ oauth.js
@@ -2,7 +2,7 @@ var ids = {
 	facebook: {
 	 clientID: process.env.FacebookID || "1395741020738264",
 	 clientSecret: process.env.FacebookSecret || "96570e43a67e3c94a75da9d2a6c32ec1",
-	 callbackURL: 'https://agile-ocean-6560.herokuapp.com/twotes'
+	 callbackURL: 'https://agile-ocean-6560.herokuapp.com/auth/facebook/callback'
 	}
 }
 
